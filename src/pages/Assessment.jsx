dc3584af73895557155bbb52756f153abf00af9e
preetash88 <sahupreetamkumar@gmail.com>
2026-01-15 17:52:27 +0530

    added more data to english.json/hindi.json

--- src/pages/Assessment.jsx
+++ src/pages/Assessment.jsx
@@ -84,7 +84,7 @@ export default function Assessment() {
         <button
           onClick={() => navigate(-1)}
           className={`flex items-center gap-2 text-lg text-gray-500
-            hover:text-gray-900 font-semibold transition mb-6
+            hover:text-gray-900 font-semibold transition mb-6 cursor-pointer
             ${isSubmitting ? "pointer-events-none opacity-40" : ""}`}
         >
           <ArrowLeft className="w-6 h-6" />
